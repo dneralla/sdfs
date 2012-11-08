be7f078c36b198fe1a26a166b0abab5026458eea
Deepak Neralla <deepak.neralla@gmail.com>
2012-11-07 20:11:01 -0600

    Delete,Get and Put File Message skeltons

---
+++ sdfs/src/edu/illinois/cs425/mp3/messages/GetFileMessage.java
@@ -0,0 +1,29 @@
+package edu.illinois.cs425.mp3.messages;
+
+import java.io.Serializable;
+
+import edu.illinois.cs425.mp3.MemberNode;
+import edu.illinois.cs425.mp3.Process;
+
+public class GetFileMessage extends Message implements Serializable{
+	
+	String sdfsFileName;
+	
+	public GetFileMessage(MemberNode sourceNode, MemberNode centralNode,
+			MemberNode alteredNode,char[] chunkContent,int chunkId,String sdfsFileName)
+	{
+		super(sourceNode, centralNode,alteredNode);
+		this.sdfsFileName=sdfsFileName;
+	   
+	}
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void processMessage(Process process) 
+	{
+
+		
+	}
+
+}
